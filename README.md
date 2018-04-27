d24c9e50261d7c0e9008ce2e284e1b1bc9afc7f3
ddiako <dd@cerber.localdomail>
2018-04-27 10:04:15 +0200

    Functional first commit

---
+++ README.md
@@ -0,0 +1,53 @@
+PREQUISITE
+==========
+
+Install figlet
+
+```
+sudo apt install figlet
+```
+
+or
+
+```
+sudo yum install figlet
+```
+
+USAGE
+=====
+
+./fancyprompt.bash
+
+
+INSTALLATION
+============
+
+The generated files must be inserted in /etc.
+Please note /etc/issue uses special command to dynamically add informations.
+The preview will show you the specific command but will not replace them as seen after a SSH.
+See below for syntax help.
+
+Information on network interface should be adapted to your needs.
+
+
+SYNTAX FOR /etc/issue
+=====================
+
+\b   Insert the baudrate of the current line.
+\d   Insert the current date.
+\s   Insert the system name, the name of the operating system.
+\l   Insert the name of the current tty line.
+\m   Insert the architecture identifier of the machine, e.g., i686.
+\n   Insert the nodename of the machine, also known as the hostname.
+\o   Insert the domainname of the machine.
+\r   Insert the release number of the kernel, e.g., 2.6.11.12.
+\t   Insert the current time.
+\u   Insert the number of current users logged in.
+\U   Insert the string "1 user" or "<n> users" where <n> is the
+     number of current users logged in.
+\v   Insert the version of the OS, e.g., the build-date etc.
+
+LICENCE
+=======
+
+MIT.
---
+++ fancyprompt.bash
@@ -0,0 +1,60 @@
+#/bin/bash
+# This script is released under the MIT license, see https://github.com/ddiako/fancyprompt
+
+outdir="out"
+tmpldir="tmpl"
+
+if [ -d $outdir ]
+then
+    echo -e "\n[1;31m*** ERROR: Your output directory doesn't exist ! ***[0m"
+    exit 1
+fi
+if [ -d $tmpldir ]
+then
+    echo -e "\n[1;31m*** ERROR: Your template directory doesn't exist ! ***[0m"
+    exit 1
+fi
+
+# SERVER NAME
+read -p "Enter the server name: " sname
+if [[ "x$sname" == "x" ]]
+then
+    echo -e "\n[1;31m*** ERROR: Server name is mandatory ! ***[0m"
+    exit 1
+fi
+
+# RELEASE INFO
+read -p "Enter the release info or press enter for autodiscover: " rinfo
+if [[ "x$rinfo" == "x" ]]
+then
+    releasepart=`lsb_release -d | cut -d ':' -f 2 | sed 's/\t//g'`
+else
+    releasepart=$rinfo
+fi
+
+{
+    # MOTD generation
+    motdpart=`figlet -f "fonts/3D Diagonal" "$sname"`
+    # Templating with tags in template noted ${VAR} or $VAR
+    MOTD_ART=$motdpart envsubst < $tmpldir/motd > $outdir/motd
+
+    # ISSUE generation
+    issuepart=`figlet -f "fonts/big" "$sname"`
+    ISSUE_ART=$issuepart OS_DESC=$releasepart envsubst < $tmpldir/issue > $outdir/issue
+} 2>/dev/null || { 
+    echo -e "\n[1;31m*** ERROR: Please install 'figlet' before using this script ! ***[0m"
+    exit 1 
+}
+
+echo -e "\n[1;32m* SUCCESS: You will find your generated files in <$outdir>.\n* Place them in </etc> when ready.[0m"
+
+read -p "Preview the files ? [y,N] " preview
+if [[ "$preview" == "y" ]]
+then
+    echo "=== MOTD ==="
+    cat $outdir/motd
+    echo "--------------------------------------------------------"
+    echo -e "\n=== ISSUE ==="
+    cat $outdir/issue
+fi
+
